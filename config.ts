78c8969b4485dc06bf7d7fb87c947c853c59dbfc
Szymon Morawski <morawski.szymon@gmail.com>
2024-12-20 20:07:20 +0100

    init

---
+++ config.ts
@@ -0,0 +1,15 @@
+type TranslationFile = {
+  name: string;
+  path: string;
+};
+
+export const translationFiles: TranslationFile[] = [
+  {
+    name: "en",
+    path: "./examples/en.json",
+  },
+  {
+    name: "pl",
+    path: "./examples/pl.json",
+  },
+];
